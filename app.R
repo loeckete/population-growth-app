0aeb5532e971cbd3c15f9ab64654b211b2ef55eb
Loecke <loecke@iastate.edu>
2026-09-22 10:26:56 -0500

    added google analytic tag

--- app.R
+++ app.R
@@ -1,3 +1,13 @@
+<!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-RWVG6BYSDE"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    
+    gtag('config', 'G-RWVG6BYSDE');
+    </script>
+      
 library(shiny)
 library(ggplot2)
 
